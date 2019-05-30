a6079ea82f6202d8d37cc974d8df6ba6e380121b
Daniel Dimitrov <ninjares@abv.bg>
2019-05-31 02:29:35 +0300

    excrecises and git headache

---
+++ c# advanced/c# advanced/30.05 - files excercises/Word Count/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+namespace Word_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texttocheck = File.ReadAllText(@"..\..\..\Text\text.txt");
+            string[] wordstocheck = File.ReadAllText(@"..\..\..\Text\words.txt").Split(new string[] {"\r", "\n"}, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine(texttocheck + '\n' + string.Join(" ", wordstocheck));
+        }
+    }
+}
