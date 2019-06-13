725723181a5d83a9089173983410e67d5e1e1a84
Daniel Dimitrov <ninjares@abv.bg>
2019-06-13 22:20:33 +0300

    generic excercises

---
+++ c# advanced/c# advanced/generics/excercises/count method strings/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace count_method_strings
+{
+    class Program
+    {
+        public class Box<T>
+        {
+            public T item;
+
+            public Box(T item)
+            {
+                this.item = item;
+            }
+
+            public override string ToString()
+            {
+                return $"{typeof(T)}: {item}";
+            }
+        }
+        class comparer<T> where T: IComparable
+        {
+            public static int compare(List<T> list, T compareto)
+            {
+                int count = 0;
+                foreach(T cmp in list)
+                {
+                    if (cmp.CompareTo(compareto) == 1) count++;
+                }
+                return count;
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<string> collection = new List<string>();
+            int n = int.Parse(Console.ReadLine());
+            while (n != 0)
+            {
+                collection.Add(Console.ReadLine());
+                n--;
+            }
+
+            Console.WriteLine(comparer<string>.compare(collection, Console.ReadLine()));
+        }
+    }
+}
---
+++ c# advanced/c# advanced/generics/excercises/tuple/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] personInfo = Console.ReadLine().Split();
+            string[] personBEERInfo = Console.ReadLine().Split();
+            string[] numbersInfo = Console.ReadLine().Split();
+
+
+            string personName = $"{personInfo[0]} {personInfo[1]}";
+            string personTown = personInfo[2];
+
+            var person = new Tuple<string, string>(personName, personTown);
+
+            string beerName = personBEERInfo[0];
+            int liters = int.Parse(personBEERInfo[1]);
+
+            var beer = new Tuple<string, int>(beerName, liters);
+
+            int n1 = int.Parse(numbersInfo[0]);
+            double n2 = double.Parse(numbersInfo[1]);
+
+            var numbers = new Tuple<int, double>(n1, n2);
+            Console.WriteLine($"{person.GetInfo()}\n{beer.GetInfo()}\n{numbers.GetInfo()}");
+
+
+        }
+
+    }
+}
