7928d69911a381786717e22f6e6fcc625aeccd49
Daniel Dimitrov <ninjares@abv.bg>
2019-11-22 04:11:38 +0200

    Up for homework

---
+++ C# DB/Entity Framework Core/11.18-11.19/XMLDemo/XMLDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+
+namespace XMLDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(File.ReadAllText(@"./../../../Library.xml"));
+            XDocument document = XDocument.Load(@"./../../../Library.xml");
+            document.Declaration.Version = "3.0";
+            var elements = document.Root.Elements();
+            foreach (var element in elements.Attributes())
+                Console.WriteLine(element);
+            document.Save(@"./../../../Library.xml");
+            
+            Console.WriteLine();
+            
+        }
+    }
+}
