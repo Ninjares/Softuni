41535988c8917014a5e3c51e53bd3169c08ed274
NInjares <daniel.dimitrov.0515@gmail.com>
2023-07-19 09:21:59 +0300

    removed node_modules libraries

--- Installation and stuff/delete command (CHECK BEFORE USE).bat
+++ Installation and stuff/delete command (CHECK BEFORE USE).bat
@@ -1,4 +1,4 @@
-CD "C:\Users\mo2-bawk\Desktop\Softuni rep\TestFolder"
+CD "C:\Users\throw\Desktop\Softuni"
 FOR /D /r %%f in (bin) DO ( 
 del /S /Q "%%f\*"
 RD /S /Q "%%f"
@@ -11,5 +11,12 @@ FOR /D /r %%f in (.vs) DO (
 del /S /Q "%%f\*" 
 RD /S /Q "%%f"
 )
+FOR /D /r %%f in (node_modules) DO ( 
+del /S /Q "%%f\*" 
+RD /S /Q "%%f"
+)
+
+
+
 
 set /p DUMMY=Hit ENTER to continue...
